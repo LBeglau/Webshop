a2a7477696d11846cdf21838ce47f0836ad61332
Leon Carlo Beglau <Leon.Beglau@mail.schwarz>
2022-02-03 11:46:52 +0100

    added ajax call to provide BuyController class with a productId; wrote code line to extract productId from request bag

--- src/Controller/BuyController.php
+++ src/Controller/BuyController.php
@@ -3,17 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BuyController extends AbstractController
 {
     /**
-     * @Route("/buy/{product}", name="buy", requirements={"page"="/d+"})
+     * @Route("/buy/{pId}", name="buy")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        /** Hier Service Klasse aufrufen in der Query gebaut werden soll -> $request an die Service Klasse übergeben und die ProductId wie unten zu sehen rausziehen **/
+        $productIdForQuery = $request->attributes->get('pId');
 
-        return $this->redirect($this->generateUrl('/'));
+        return $this->json('success');
     }
 }
